a3b5c65528ddd030b57eddb0e7bb04876bcc8a42
Kevin <kz363@nyu.edu>
2014-05-03 18:14:58 -0400

    updated ruby triangle debugger

--- week_8_and_9/4_Ruby/triangle_debugger/my_solution.rb
+++ week_8_and_9/4_Ruby/triangle_debugger/my_solution.rb
@@ -1,39 +1,42 @@
 # U3.W8-9: Triangle Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 
 # 1. Original Solution
 
-def valid_triangle?(a, b, c, sum)
-  if a != 0 || b != 0 || c != 0
-  if a >= b
-  largest = a
-  sum += b
-  else largest = b
-  sum += a
-  end
-  if c > largest
-  sum += largest
-  largest = c
-  else sum += c
-  end
-  if sum > largest
-  return "true"
-  else return "false"
-  end
-  else return "false"
-  end
-end
+# def valid_triangle?(a, b, c, sum)
+#   if a != 0 || b != 0 || c != 0
+#   if a >= b
+#   largest = a
+#   sum += b
+#   else largest = b
+#   sum += a
+#   end
+#   if c > largest
+#   sum += largest
+#   largest = c
+#   else sum += c
+#   end
+#   if sum > largest
+#   return "true"
+#   else return "false"
+#   end
+#   else return "false"
+#   end
+# end
 
 
 # 2. Answer the question for each bug
 
 # --- Bug 1 ---
 # * what is the exact description of the error?
+# ArgumentError
 # * what is the exact line number the error is appearing?
+# 9
 # * before you fix the bug, what do you think is causing the error?
+# sum shouldn't be in the parameters
 
 # --- Bug 2 ---
 # * what is the exact description of the error?
@@ -47,7 +50,31 @@ end
 
 
 # 3. Refactored Solution (Comment the other code to run this)
-
+def valid_triangle?(a, b, c)
+  sum = 0
+  if a != 0 || b != 0 || c != 0
+    if a >= b
+      largest = a
+      sum += b
+    else 
+      largest = b
+      sum += a
+    end
+    if c > largest
+      sum += largest
+      largest = c
+    else 
+      sum += c
+    end
+    if sum > largest
+      return true
+    else 
+      return false
+    end
+  else 
+    return false
+  end
+end
 
 
 
@@ -56,6 +83,13 @@ end
 
 
 
-
-
 # 5. Reflection 
+# I had trouble figuring out what exactly I was supposed to answer in the questions about the bugs. Running
+# the rspec tests only returns on "real" error which is the ArgumentError. Everything else after that just 
+# fails because the rest of the code relies on that 4th argument which the tests don't give. It seems to me
+# that you can only find out what the other bugs are AFTER fixing one bug, so that's what I did while refactoring
+# the old code. After fixing the argument bug, the only tests that failed were when an argument equaled 0 and 
+# when it was an impossible triangle. It took me a while to figure out why because I kept double-checking the
+# logic and I thought it sounded fine in my head. Then I realized true/false where strings instead of booleans,
+# and I laughed a bit. I don't know why the other tests passed but tiny mistakes like those happen in code all
+# the time and they're hard to find.
